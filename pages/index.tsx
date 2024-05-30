acf543a946fc04b57bd89df3fb1d5b19439c9357
Hyunsoo_kim <red_panda119@naver.com>
2024-05-31 08:52:37 +0900

    Ui, Feat : 캐러셀 Ui, 일부 기능 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
-import styles from "@/styles/Home.module.css";
+import React, { useState, useEffect } from "react";
+
+import Carousel from "@/componenets/carousel";
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
+  const [viewPort, setViewPort] = useState<any>("");
+
+  useEffect(() => {
+    const handleResizing = () => {
+      if (window.innerWidth < 768) {
+        setViewPort("mobile");
+      } else if (window.innerWidth >= 768 && window.innerWidth < 1025) {
+        setViewPort("tablet");
+      } else if (window.innerWidth >= 1025) {
+        setViewPort("desktop");
+      }
+    };
+
+    window.addEventListener("resize", handleResizing);
+    handleResizing();
+
+    return () => {
+      window.removeEventListener("resize", handleResizing);
+    }
+  });
   return (
-    <div></div>
+    <div className={styles["wrapper"]}>
+      <Carousel viewPort={viewPort} />
+    </div>
   );
 }
