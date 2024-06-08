c018e8caaddd482fbc47e705f9ed3b4641e7d2bd
Hyunsoo_kim <red_panda119@naver.com>
2024-06-08 12:18:55 +0900

    Ui : 메인페이지의 각 카테고리 섹션 통일 컴포넌트 생성

---
+++ componenets/mainpage-section/index.tsx
@@ -0,0 +1,68 @@
+import { LuMegaphone } from "react-icons/lu"; //메가폰
+import { TbNews } from "react-icons/tb";
+import { ImNewspaper } from "react-icons/im"; //신문지
+import { GiNothingToSay } from "react-icons/gi"; //동그란 말풍선
+import { SlSpeech } from "react-icons/sl"; //네모난 말풍선
+import { FaRegLightbulb } from "react-icons/fa"; //전구
+import { GiGrandPiano } from "react-icons/gi"; //피아노
+import { CiSearch } from "react-icons/ci"; //돋보기
+
+import { MainPageSectionProps } from "@/types/mainpage-section";
+import styles from "./index.module.scss";
+
+const MainPageSection = ({ viewPort, category, detail, data }: MainPageSectionProps) => {
+  return (
+    <div className={styles["wrapper"]}>
+      <div className={styles["category"]}>{category}</div>
+      <div className={styles["list"]}>
+        {viewPort === "mobile"
+          ? data.map((item: any, index: number) => {
+              return (
+                index < 3 && (
+                  <div className={styles["list__item"]} key={index}>
+                    <p className={styles["category-detail"]}>{detail}</p>
+                    {category === "Notice" ? (
+                      <LuMegaphone size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "News" ? (
+                      <TbNews size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "Board" ? (
+                      <GiNothingToSay size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "Promote" ? (
+                      <GiGrandPiano size={17} style={{ flexShrink: 0 }} />
+                    ) : (
+                      <CiSearch size={17} style={{ flexShrink: 0 }} />
+                    )}
+                    <p className={styles["title"]}>{item.title}</p>
+                    <p className={styles["explan"]}>{item.text}</p>
+                  </div>
+                )
+              );
+            })
+          : data.map((item: any, index: number) => {
+              return (
+                index < 4 && (
+                  <div className={styles["list__item"]} key={index}>
+                    <p className={styles["category-detail"]}>{detail}</p>
+                    {category === "Notice" ? (
+                      <LuMegaphone size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "News" ? (
+                      <TbNews size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "Board" ? (
+                      <GiNothingToSay size={17} style={{ flexShrink: 0 }} />
+                    ) : category === "Promote" ? (
+                      <GiGrandPiano size={17} style={{ flexShrink: 0 }} />
+                    ) : (
+                      <CiSearch size={17} style={{ flexShrink: 0 }} />
+                    )}
+                    <p className={styles["title"]}>{item.title}</p>
+                    <p className={styles["explan"]}>{item.text}</p>
+                  </div>
+                )
+              );
+            })}
+      </div>
+    </div>
+  );
+};
+
+export default MainPageSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 
+import { notice, news, board, promote, job } from "@/dummy-data/mainpage-section-dummydata";
 import Carousel from "@/componenets/carousel";
-import styles from "@/styles/Home.module.scss";
 import NoticeSection from "@/componenets/noticeSection";
+import MainPageSection from "@/componenets/mainpage-section";
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
   const [viewPort, setViewPort] = useState<any>("");
@@ -23,12 +25,21 @@ export default function Home() {
 
     return () => {
       window.removeEventListener("resize", handleResizing);
-    }
+    };
   });
   return (
     <div className={styles["wrapper"]}>
       <Carousel viewPort={viewPort} />
-      <NoticeSection viewPort={viewPort} />
+      <div className={styles["section-wrapper"]}>
+        <div className={styles["section"]}>
+          <MainPageSection viewPort={viewPort} category="Notice" detail="공지" data={notice} />
+          <MainPageSection viewPort={viewPort} category="News" detail="국내" data={news} />
+        </div>
+        <div className={styles["section"]}>
+          <MainPageSection viewPort={viewPort} category="Board" detail="자유게시판" data={notice} />
+          <MainPageSection viewPort={viewPort} category="Promote" detail="밴드홍보" data={news} />
+        </div>
+      </div>
     </div>
   );
 }
