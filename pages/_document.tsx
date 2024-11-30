42289c2c43ad420024a8bc39f4976af89043e069
Hyunsoo_kim <red_panda119@naver.com>
2024-11-30 18:01:50 +0900

    Style : font-family: "Varela Round", sans-serif; 도 _document 스크립트 link 태그에 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Metrophobic&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Metrophobic&family=Noto+Sans+KR:wght@100..900&family=Varela+Round&display=swap" rel="stylesheet" />
       </Head>
       <body style={{ position: "relative" }}>
         <Main />
