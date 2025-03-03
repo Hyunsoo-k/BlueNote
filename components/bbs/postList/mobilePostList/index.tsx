d5855b4638139233468d939977e76429670e8772
Hyunsoo_kim <red_panda119@naver.com>
2025-03-03 22:11:58 +0900

    Ui : 모바일 버전의 공지사항, 자유게시판, 구인구직 섹션 ui 변경

--- components/bbs/postList/mobilePostList/index.tsx
+++ components/bbs/postList/mobilePostList/index.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef, useEffect } from "react";
-
+import { RiThumbUpLine } from "react-icons/ri";
+import { FaRegCommentDots } from "react-icons/fa6";
+import { AiOutlineEye } from "react-icons/ai";
 import { useGetMobileBoardQuery } from "@/hooks/bbs/useGetMobileBoardQuery";
 import { formatYM } from "@/utils/dateFormatter";
 
@@ -18,11 +21,13 @@ const MobilePostList = ({ initialData, resolvedUrl, viewport }: Props) => {
   const lastElementRef = useRef<HTMLLIElement | null>(null);
 
   const {
-    isFetchingNextPage,
+    isFetching,
     fetchNextPage,
     hasNextPage,
     data: queryData,
   } = useGetMobileBoardQuery(initialData.mainCategory, resolvedUrl, initialData);
+
+  console.log(queryData);
 
   useEffect(() => {
     const io = new IntersectionObserver(
@@ -49,34 +54,84 @@ const MobilePostList = ({ initialData, resolvedUrl, viewport }: Props) => {
 
   if (viewport === "mobile") {
     return (
-      <>
-        <ul className={styles["container"]}>
-          {queryData?.pages?.map((page: any, pageIndex: number) =>
-            page?.postList?.map((post: any, index: number) => (
-              <li
-                key={(pageIndex + 1) * index}
-                ref={(pageIndex === queryData?.pages?.length - 1 && index === page?.postList?.length - 1) ? lastElementRef : null}
-                onClick={(e) => handleClickItem(e, post.mainCategory, post._id)}
-                className={styles["element"]}
-              >
-                <div className={styles["top"]}>
-                  <span className={styles["top__title"]}>{post.title}</span>
-                  {!!post.commentCount && (
-                    <span className={styles["top__comment"]}>({post.commentCount})</span>
-                  )}
+      <ul className={styles["container"]}>
+        {!isFetching && !queryData.pages[0].totalPostCount && (
+          <p className={styles["not-found-message"]}>등록된 게시글이 없습니다.</p>
+        )}
+        {queryData?.pages?.map((page: any, pageIndex: number) =>
+          page?.postList?.map((post: any, index: number) => (
+            <li
+              key={(pageIndex + 1) * index}
+              ref={(pageIndex === queryData?.pages?.length - 1 && index === page?.postList?.length - 1) ? lastElementRef : null}
+              onClick={(e) => handleClickItem(e, post.mainCategory, post._id)}
+              className={styles["element"]}
+            >
+              <div className={styles["writing-info"]}>
+                <span className={styles["writer"]}>{post.writer.nickname}</span>
+                <span className={styles["boundary-dot"]}></span>
+                <span className={styles["created-at"]}>{formatYM(post.createdAt)}</span>
+                <span className={styles["boundary-dot"]}></span>
+                <span className={styles["sub-category"]}>{post.subCategory}</span>
+              </div>
+              {post.thumbnailSrc ? 
+                <div className={styles["having-thumbnail-post-main"]}>
+                  <div className={styles["title-wrapper"]}>
+                    <p className={styles["title"]}>{post.title}</p>
+                  </div>
+                  <div className={styles["content-wrapper"]}>
+                    <p className={styles["content"]}>{post.content}</p>
+                  </div>
+                  <Image
+                    src={post.thumbnailSrc}
+                    alt=""
+                    width={120}
+                    height={110}
+                    className={styles["thumbnial"]}
+                  />
+                </div> :
+                <div className={styles["not-having-thumbnail-post-main"]}>
+                  <div className={styles["title-wrapper"]}>
+                    <p className={styles["title"]}>{post.title}</p>
+                  </div>
+                  <div className={styles["content-wrapper"]}>
+                    <p className={styles["content"]}>{post.content}</p>
+                  </div>
                 </div>
-                <div className={styles["bottom"]}>
-                  <span className={styles["bottom__writer"]}>{post.writer.nickname}</span>
-                  <span className={styles["bottom__views"]}>조회 {post.views}</span>
-                  <span className={styles["bottom__recommend"]}>추천 {post.recommend.length}</span>
-                  <span className={styles["bottom__sub-category"]}>{post.subCategory}</span>
-                  <span className={styles["bottom__created-at"]}>{formatYM(post.createdAt)}</span>
+              }
+              <div className={styles["post-info-box"]}>
+                <div className={styles["views-wrapper"]}>
+                  <AiOutlineEye
+                    size={16}
+                    color="#fff"
+                  />
+                  <span className={styles["views"]}>
+                    {post.views}
+                  </span>
                 </div>
-              </li>
-            ))
-          )}
-        </ul>
-      </>
+                <div className={styles["comment-wrapper"]}>
+                  <FaRegCommentDots
+                      size={15}
+                      color="#fff"
+                      style={{ position: "relative", top: "0px" }}
+                    />
+                  <span className={styles["comment"]}>
+                    {post.commentCount}
+                  </span>
+                </div>
+                <div className={styles["recommend-wrapper"]}>
+                  <RiThumbUpLine
+                    size={15}
+                    color="#fff"
+                  />
+                  <span className={styles["recommend"]}>
+                    {post.recommend.length}
+                  </span>
+                </div>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
     );
   }
 };
