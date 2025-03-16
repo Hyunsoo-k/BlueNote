b53fcf0cc38864e2ba42f8cae32e40e61bbed3a5
Hyunsoo_kim <red_panda119@naver.com>
2025-03-16 23:27:35 +0900

    Refactor : UseCreateReply 훅 타입 리팩토링

--- hooks/bbs/useCreateReply.ts
+++ hooks/bbs/useCreateReply.ts
@@ -1,9 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { instance } from "@/axios"
 import { queryKey } from "@/queryKey";
 
-const createReplyFn = async (comment_id: string, requestBody: any) => {
+interface RequestBodyType {
+  post_id: string;
+  comment_id: string;
+  content: string;
+};
+
+const createReplyFn = async (comment_id: string, requestBody: RequestBodyType) => {
   const response = await instance.post(`${window.location.pathname}/comment/${comment_id}/reply`, requestBody);
 
   return response.data;
@@ -12,14 +19,15 @@ const createReplyFn = async (comment_id: string, requestBody: any) => {
 const useCreateReply = (
   post_id: string,
   comment_id: string,
-  setShowCreateReply: any
+  setIsCreateReplyOpen: Dispatch<SetStateAction<boolean>>
 ) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (requestBody: any) => createReplyFn(comment_id, requestBody),
+    mutationFn: (requestBody: RequestBodyType) => createReplyFn(comment_id, requestBody),
     onSuccess: () => {
-      setShowCreateReply(false);
+      setIsCreateReplyOpen(false);
+      
       queryClient.invalidateQueries({ queryKey: queryKey.post(post_id) });
     },
     onError: (e: any) => { console.log(e);}
