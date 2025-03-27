69ae7e790629982f19565907584c2516a9efd464
Hyunsoo_kim <red_panda119@naver.com>
2025-03-27 17:10:20 +0900

    UI : 반응형 구현

--- pages/myPage/schedule/index.tsx
+++ pages/myPage/schedule/index.tsx
@@ -1,18 +1,23 @@
-import { TbMoodSadSquint } from "react-icons/tb";
+import { LuConstruction } from "react-icons/lu";
 
+import { useGetViewport } from "@/hooks/viewport";
 import MyPageMenu from "@/components/myPageMenu";
 
 import styles from "./index.module.scss";
 
 const SchedulePage = () => {
+  const viewport = useGetViewport();
+
   return (
     <div className={styles["schedule-page"]}>
-      <MyPageMenu currentPage="내 일정" />
+      {viewport !== "mobile" && (
+        <MyPageMenu currentPage="내 일정" />
+      )}
       <div className={styles["schedule-page__content"]}>
         <h1 className={styles["schedule-page__title"]}>내 스케줄</h1>
         <div className={styles["schedule__shcedule"]}>
-          <TbMoodSadSquint size={300} color="rgb(48, 140, 204)" />
-          <p>준비중 입니다</p>
+          <LuConstruction size={200} color="rgb(11, 66, 122)" />
+          <p className={styles["explantion"]}>준비중 입니다</p>
         </div>
       </div>
     </div>
